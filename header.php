37099a159224d028d45a582a4b36e6ebac1f7d4d
Sara Herman <saraherman92@gmail.com>
2015-02-05 16:07:46 -0800

    Responsive Update 1
    responsive navbar, logos, animation.
    Still needs work

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
 	<meta charset="UTF-8">
 	<title><?php wp_title( '', true, 'right' ); ?></title>
 	
+	<meta name="viewport" content="width=device-width">
+	
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet"><link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,700,300,600,800,400' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/normalize.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
@@ -12,29 +14,21 @@
 </head>
 <body>
 	<header>
-		<div class="responsive header">
-			<div class="container">
-				<nav>
-					<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'full navbar' ) ); ?>
-				</nav>
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-white.png">
-				<p><?php $blog_title = get_bloginfo('name'); echo $blog_title;?><p>
-				<nav>
-					<?php wp_nav_menu( array( 'theme_location' => 'extra-menu', 'container_class' => 'navbar' ) ); ?>
-				</nav>
-			</div>	
-		</div>
 		<div class="first header">
 			<div class="container">
+				
 				<div class="col-wrap">
-					<nav class="column half left">
-							<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'full navbar' ) ); ?>
+					<nav class="column left">
+						<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'full navbar' ) ); ?>
 					</nav>
-					<div class="column half right">
-						<nav>
-							<?php wp_nav_menu( array( 'theme_location' => 'extra-menu', 'container_class' => 'navbar' ) ); ?>
-						</nav>
+					
+					<div class="column responsive-logo">
+							<h2><i class="fa fa-bars"></i><?php $blog_title = get_bloginfo('name'); echo $blog_title;?></h2>
 					</div>
+				
+					<nav class="column right">
+						<?php wp_nav_menu( array( 'theme_location' => 'extra-menu', 'container_class' => 'navbar' ) ); ?>
+					</nav>
 				</div>	
 			</div>
 		</div>
--- script.js
+++ script.js
@@ -1,68 +1,72 @@
 $(document).ready(function(){
+	// if post has image adds a min height so the image doesnt overflow
 	$('.post:has(img)').css({'min-height':'270px'});
 
+	// adds down chevron when li has dropdown elements
 	$('.menu-item-has-children >a').append('&nbsp;<i class="fa fa-chevron-down"></i>');
 
 	function scrollY() {
 		return window.pageYOffset || document.documentElement.scrollTop;
 	}
-
+	// scroll function for responsive header
 	$(window).scroll(function () {
-		if (scrollY() < 10){
-			$('.responsive.header').stop( true, true ).hide();
+		//shuts everything the fuck up on scroll 
+		$('*').stop(true, true);
+		// on scroll pos === top
+		if (scrollY() ===0){
+			$('.responsive-logo').css({'opacity':'0'});
 
 			$('main').stop( true, true ).animate({marginTop: '260px'}, function(){
 
 			});
 			$('.first.header').stop( true, true ).slideDown();
-			$('.spec-wrap').stop( true, true ).slideDown(250, function(){
-				$('.logo').stop( true, true ).show(function(){
-					$('.logo-text').stop( true, true ).hide();
-					$('.logo').stop( true, true ).css({ 'z-index': '-1'}).stop( true, true ).animate({top:'10px'}, 500, function(){
-						$('.logo').stop( true, true ).delay(1000).css('z-index','0');
-					$('.logo-text').stop( true, true ).fadeIn();
+			$('.spec-wrap').slideDown(250, function(){
+				$('.logo').show(function(){
+					$('.logo-text').hide();
+					
+					$('.logo').css({ 'z-index': '-1'}).animate({top:'10px'}, 500, function(){
+						$('.logo').delay(1000).css('z-index','0');
+					$('.logo-text').fadeIn();
 					});
 					
 				})
 			});
 			 
 		}
-		else if (scrollY() != 0 || scrollY() > 10) {
+		// on scroll pos === not top
+		else if (scrollY() != 0 || scrollY() > 1) {
 			$('.logo-text').stop( true, true ).fadeOut(250, function(){
 				$('.logo').stop( true, true ).css({'z-index': '-1'}).stop( true, true ).animate({top:'103px'}, 500, function(){
 					$('.logo').stop( true, true ).hide();
-					$('.spec-wrap').stop( true, true ).slideUp(250);
+					$('.spec-wrap').stop( true, true ).hide();
 					$('.first.header').stop( true, true ).animate({}, function(){
-						$('main').stop( true, true ).animate({marginTop: '132px'});
-
+						$('main').stop( true, true ).animate({marginTop: '222px'});
+						$('.responsive-logo').css({'opacity':'1'});
 					});
 					
 				});
 
 			});
 		}
-		// if ($('.spec-wrap').css('padding-top') != '138px') {
-  // 			$('.logo-text').stop( true, true ).fade(500);
-		// }
+		
 	});
 
-	// $(window).scroll(function(){
-	//     if($(this).scrollTop() > 0) {
-	//         $('.logo-text').fadeOut(500,function(){
-	//         	$('.spec-wrap').animate({ "padding-top": "20px" }, "slow", function(){
-	//         		$('.sidebar > div').addClass('fixed');
-	//         	} );
-	        	
-	//         });
-	        
-	//     }
-	//     else {
-	//     	 $('.sidebar > div').removeClass('fixed');
-	//     	 $('.spec-wrap').animate({ "padding-top": "138px" }, "slow",function(){
-	//     	 	$('.logo-text').fadeIn();
-	    	 	
-	//     	 } );
-	    	
-	//     }
-	// });     
+	// fluid resizing of header navs to fit .responsive header without having it fixed pos
+		var containerWidth = $('.container').width();
+		var logoWidth = $('.responsive-logo').width();
+		var navWidth = Math.floor((containerWidth - logoWidth)/2-1);
+		$('.navbar').css({width:navWidth});
+		console.log('container:'+containerWidth+', logo:'+logoWidth+', nav:'+navWidth);
+	// makes responsive on resize
+	$( window ).resize(function() {
+		var containerWidth = $('.container').width();
+		var logoWidth = $('.responsive-logo').width();
+		var navWidth = Math.floor((containerWidth - logoWidth)/2-1);
+		$('.navbar').css({width:navWidth});
+		console.log('On Resize::: container:'+containerWidth+', logo:'+logoWidth+', nav:'+navWidth);
+	});
+
+
+
+	
 });
